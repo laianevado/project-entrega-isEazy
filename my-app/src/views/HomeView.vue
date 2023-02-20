2743da516c88501e50a2fb2ba4d7a09a8ec57a1f
laianevado <laianevado@gmail.com>
2023-02-20 21:34:56 +0100

    Project vue + tailwind

---
+++ my-app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import Modal from '../components/Modal.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <Modal/>
+  </main>
+</template>
